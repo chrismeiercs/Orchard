4d3740bbb98cc0b4941e291bfcfbd5f42f8acd81
seaadmin <seaadmin@SEA2014>
2015-08-05 15:30:45 -0400

    Transformation CSV export complete

--- src/orchard/OrchardExportCSV.java
+++ src/orchard/OrchardExportCSV.java
@@ -23,25 +23,30 @@ import javax.swing.ListModel;
 public class OrchardExportCSV {
 
     private String csvFileName;
-    private List queryIDs;
-    //private String queryIDs;
+    private ListModel queryIDs;
     private Connection dbConnection;
+    private CSVWriter writer;
 
     public OrchardExportCSV(String fileName, JList IDs) throws IOException, SQLException {
         DatabaseConnector dbConnector = new DatabaseConnector() ;
-        Connection dbConnection = null;
+        dbConnection = null;
         try {
            
             dbConnection = dbConnector.connectDB();
+            System.out.println(dbConnection);
         } catch (Exception e) {
             System.out.println("Unable to connect to database: " + e);
         }
 
         try {
             csvFileName = fileName;
-            //Object queryIDs = new Object[IDs.getSelectedValues().length];
-            queryIDs = IDs.getSelectedValuesList();
+            
+            queryIDs = IDs.getModel();
+            
+            System.out.println("About to write csv");
             writeCSV();
+            writer.close();
+             System.out.println("CSV Written");
         } catch (Exception e) {
             System.out.println("Exception: " + e);
         } finally {
@@ -50,38 +55,81 @@ public class OrchardExportCSV {
     }
 
     private void writeCSV() throws IOException, SQLException {
-        CSVWriter writer = new CSVWriter(new FileWriter(csvFileName + ".csv"));
+         writer = new CSVWriter(new FileWriter("csvFileName" + ".csv"));
 
-        for (Object id : queryIDs) {
-            writer.writeNext(retrieveInfo(id.toString()));
+        for(int i =0; i < queryIDs.getSize();i++){
+            
+            
+            String[][] qq = retrieveInfo(queryIDs.getElementAt(i).toString());
+            for(int j=0; j< qq.length; j++){
+                String[] row = qq[j];
+                writer.writeNext(row);
+            }
+             
         }
 
     }
 
-    private String[] retrieveInfo(String id) throws SQLException {
-        String[] info = null;
+    private String[][] retrieveInfo(String id) throws SQLException {
+        
+        String[][] info = null;
         Statement stmt = null;
-        String query = "Select * from dbName where ID=" + id;
-
+        String query = "Select * from Transformation_has_Gene where Gene_Locus='" + id+"'";
+        
+        System.out.println(query);
         try {
+            
             stmt = dbConnection.createStatement();
+            
             ResultSet rs = stmt.executeQuery(query);
             int columnCount = rs.getMetaData().getColumnCount();
-
-            for (int i = 0; i < columnCount; i++) {
-                info[i] = rs.getObject(i).toString();
+            int rowCount = this.getNumberOfRows(rs) + 1;
+            info = new String[rowCount][columnCount];
+            //add in column names
+            for(int m = 1; m < columnCount+1; m++){
+                info[0][m-1] = rs.getMetaData().getColumnName(m);  
             }
+            
+            
+            
+            System.out.println("Column count: " + columnCount);
+            
+         
+            int i=1;
+            while(rs.next()){
+                for(int k = 1; k < columnCount+1; k++) {
+                    System.out.println(rs.getObject(k).toString());
+                    info[i][k-1] = rs.getObject(k).toString();
+                    
+                }
+                i++;
+            }
+            
+           
 
         } catch (SQLException e) {
 
-            System.out.println(e);
+            System.out.println("SQL " +e);
         } finally {
             if (stmt != null) {
                 stmt.close();
             }
         }
-
+        
         return info;
     }
+    
+    public int getNumberOfRows(ResultSet res){
+    int totalRows = 0;
+    try {
+        res.last();
+        totalRows = res.getRow();
+        res.beforeFirst();
+    } 
+    catch(Exception ex)  {
+        return 0;
+    }
+    return totalRows ;    
+}
 
 }
